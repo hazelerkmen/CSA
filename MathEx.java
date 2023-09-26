138f523f6c15b8d160195df1480e6526ab298e7f
hazelerkmen <144830896+hazelerkmen@users.noreply.github.com>
2023-09-26 13:59:22 -0400

    Add files via upload

--- MathEx.java
+++ MathEx.java
@@ -3,13 +3,30 @@ public class MathEx{
         System.out.println(swap(123));
         System.out.println(swap(1234));
         System.out.println(swap(123456));
+        timeLeft(1,15,4,36);
+        timeLeft(1,34,8,20);
+        dayOfWeek(0,1);
     }
 
         public static int swap(int x){
             int y = x/10%10;
             int z = x%100%10;
-            int b = x-x%100;
-            return b + z*10 + y;
+            int u = x-x%100;
+            return u + z*10 + y;
 
     }
+
+    public static void timeLeft(int curHour, int curMin, int depHour, int depMin){
+        int p=(depHour*60+depMin)-(curHour*60+curMin);
+        int hoursLeft=p/60;
+        int minLeft=p%60;
+        System.out.println(hoursLeft + " hours and " + minLeft + " minutes");
+
+    }
+
+public static int dayOfWeek(int januaryFirst, int day){
+    answer = day%7+1+day;
+    return answer;
+}
+
 }
