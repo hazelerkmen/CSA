5f97c33d431d6008a93b35d7a114ff7e0e1a7965
hazelerkmen <144830896+hazelerkmen@users.noreply.github.com>
2023-10-04 13:37:37 -0400

    Add files via upload

--- Box.java
+++ Box.java
@@ -22,11 +22,27 @@ public class Box {
         color = "white";
         hasALid = false;
     }
+    public double getHeight(){
+        return height;
+    }
+    public void setHeight(double h){
+        if(h>0){
+            height = h;
+        }
+    }
+    public double volume(){
+        double v = length * width * height;
+        return v;
+    }
 
     public static void main(String[] args){
         // create an instance
         Box b1 = new Box(6.5, 8.3, 9.4, "brown", false);
         Box b2 = new Box(3.6, 2.8, 7.4);
         Box b3 = new Box();
+        b3.setHeight(5,3);
+        System.out.println(b3.getHeight());
+        
+        
     }
 }
