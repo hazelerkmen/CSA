20bfe7ace1653af47644f596ee11998a05e32469
hazelerkmen <144830896+hazelerkmen@users.noreply.github.com>
2023-10-06 12:51:10 -0400

    Add files via upload

--- Box.java
+++ Box.java
@@ -5,16 +5,24 @@ public class Box {
     private String color;
     private boolean hasALid;
 
-    // contructor - creates/initializes an instant
+    // constructor - creates/initializes an instance
     // of a class
     public Box(double l, double w, double h, String c, boolean lid){
         length = l;
         width = w;
         height = h;
+        color = c;
+        hasALid = lid;
+    }
+    // overload the constructor
+    public Box(double l, double w, double h){
+        length = l;
+        width = w;
+        height = h;
         color = "purple";
         hasALid = true;
-// default constructor 
     }
+    // default constructor
     public Box(){
         length = 1.0;
         width = 1.0;
@@ -30,19 +38,40 @@ public class Box {
             height = h;
         }
     }
+
     public double volume(){
         double v = length * width * height;
         return v;
     }
-
+    public String toString(){
+        String s = "A box with dimensions " + length + " " + width + " " + height;
+        s+= "\nColor is " + color + " has a lid? " + hasALid;
+        return s;
+    }
+    public boolean isTaller(Box other){
+        double h = other.getHeight();
+        if (height > h) return true;
+        return false;
+    }
     public static void main(String[] args){
         // create an instance
-        Box b1 = new Box(6.5, 8.3, 9.4, "brown", false);
-        Box b2 = new Box(3.6, 2.8, 7.4);
+        Box b1 = new Box(6.5,8.3,9.4,"brown", false);
+        Box b2 = new Box(3.6, 2.8,7.4);
         Box b3 = new Box();
-        b3.setHeight(5,3);
+        b3.setHeight(5.3);
         System.out.println(b3.getHeight());
-        
-        
+        System.out.println(b1.getHeight());
+        double v1 = b1.volume();
+        System.out.println("The volume of box 1 "+v1);
+        double v2 = b2.volume();
+        System.out.println("The volume of box 2 "+v2);
+
+        // print the box
+        System.out.println(b1);
+        System.out.println(b2);
+
+        boolean f = b1.isTaller(b3);
+        System.out.println("is b1 taller than b3 " + f);
     }
+    
 }
