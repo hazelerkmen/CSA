5b88fb96b1ce9c87fc91679993770fe84a05c01b
hazelerkmen <144830896+hazelerkmen@users.noreply.github.com>
2023-10-12 13:21:17 -0400

    Add files via upload

--- Fraction.java
+++ Fraction.java
@@ -1,4 +1,4 @@
-public class Fraction {
+public class Fraction{
     private int numerator;		//attributes
 	private int denominator;
 	
@@ -37,9 +37,22 @@ public class Fraction {
     public Fraction subtract(Fraction other){
         int n = this.numerator*other.denominator - this.denominator*other.numerator;
         int d = this.denominator * other.getDenom();
+
+		Fraction result = new Fraction(n,d);
+		return result;
+
     }
 	
 	// TODO multiply()
+	public Fraction multiply(Fraction other){
+
+	int n = this.numerator*other.numerator;
+	int d = this.denominator*other.denominator;
+	Fraction result = new Fraction(n,d);
+	return result;
+
+	}
+
 	
 	// TODO toString()
     public String toString(){
@@ -49,6 +62,12 @@ public class Fraction {
 
 	
 	// TODO reciprocal()
+	public Fraction reciprocal(Fraction other){
+		int n = numerator;
+		int d = denominator;
+		Fraction result = new Fraction(d,n);
+		return result;
+	}
 	
 	
 	public static void main(String[] args) {
@@ -59,17 +78,22 @@ public class Fraction {
         Fraction f4 = f1.subtract(f2);			// add 2 fractions
 		System.out.println("3/4 + 4/5 = " + f3);	// print the answer
 			
-		/*
-		Fraction f4 = f1.subtract(f2);
+		
 		System.out.println(f1 + " - " + f2 + " = " + f4);
 		
-		Add code to multiply f1 and f2 here
+		//Add code to multiply f1 and f2 here
+		Fraction f8 = f1.multiply(f2);
+		System.out.println("3/4 * 4/5 = " + f8);
 		
-		Add code to call reciprocal here
+		//Add code to call reciprocal here
+		Fraction f9 = f1.reciprocal(f1);
+		System.out.println("The reciprocal of 3/4 is " + f9);
 		
-		create 2 new fractions and add them here.
-		
-		*/	
+		// create 2 new fractions and add them here.
+		Fraction f5 = new Fraction(1,9);
+		Fraction f6 = new Fraction(5,6);
+		Fraction f7 = f5.add(f6);
+		System.out.println("1/9 + 5/6 = " + f7);
 		
 
 	}
