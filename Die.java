4c255abd577f4e8bded985892c84cd60df16717d
hazelerkmen <144830896+hazelerkmen@users.noreply.github.com>
2023-11-21 13:58:30 -0500

    Add files via upload

---
+++ Die.java
@@ -0,0 +1,15 @@
+public class Die {
+    private int x;
+
+    public void roll(){
+        x = ((int) (Math.random()*6) + 1);
+    }
+    public int getNumDots(){
+        return x;
+    }
+    public static void main(String[] args){
+        Die die = new Die();
+        die.roll();
+        System.out.println(die.getNumDots());
+    }
+}
