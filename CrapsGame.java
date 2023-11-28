dd085b7270935e3c25a069c1bb8b644408be58ef
hazelerkmen <144830896+hazelerkmen@users.noreply.github.com>
2023-11-28 16:07:34 -0500

    Update CrapsGame.java

--- CrapsGame.java
+++ CrapsGame.java
@@ -16,30 +16,34 @@ public class CrapsGame
    */
   public int processRoll(int total)
   {
-    point = 0;
-    if (point == 0 && total==2 || total==3 || total==12){
-      result = -1;
-      point = 0;
-    }
-    if (point == 0 && total==7 || total==11){
-      result = 1;
-      point = 0;
-    }
-    if (point == 0 && total==4 || total==5 || total==6 || total==8 || total ==9 || total == 10){
-      result = 0;
-      point = total;
-    }
-    if (point != 0 && total ==point){
-      result = 1;
-      point = 0;
+    if (point == 0){
+      if (total==2 || total==3 || total==12){
+        point = 0;
+        result = -1;
       }
-    if (point != 0 && total ==7){
-      result = -1;
-      point = 0;
+      if (total==7 || total==11){
+        point = 0;
+        result = 1;
       }
-    else
-      point = total;
-   
+      if (total==4 || total==5 || total==6 || total==8 || total==9 || total==10){
+        point = total;
+        result = 0;
+      }
+    }
+    
+    if (point != 0){
+      if (total==point){
+        point = 0;
+        result = 1;
+      }
+      if (total==7){
+      point = 0;
+      result = -1;
+      }
+      if (total!=point && total!=7){
+        result = 0;
+      }
+    }
     return result;
   }
 
