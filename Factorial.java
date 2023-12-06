fd5897ea1a8475bad5c20938974c27879c8e15c6
hazelerkmen <144830896+hazelerkmen@users.noreply.github.com>
2023-12-06 13:59:05 -0500

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,14 @@
+public class Factorial {
+    public static void main(String[] args){
+        Factorial f = new Factorial();
+        System.out.println(f.calcFactorial(6));
+
+    }
+
+    public int calcFactorial(int n){
+        int multiply = n-1;
+        
+    }
+
+
+}
