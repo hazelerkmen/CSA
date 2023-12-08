37fbe0e8b85d4b9ada144d8cde31f3cb30378488
Hazel Erkmen <hazelerkmen@billericak12.com>
2023-12-08 16:45:06 +0000

    h

--- Factorial.java
+++ Factorial.java
@@ -1,13 +1,19 @@
 public class Factorial {
     public static void main(String[] args){
         Factorial f = new Factorial();
-        System.out.println(f.calcFactorial(6));
-
+        for (int n=1; n<=20; n++){
+            System.out.println(f.calcFactorial(n));
+        }
     }
 
-    public int calcFactorial(int n){
+    public long calcFactorial(int n){
         int multiply = n-1;
-        
+        long sum = n;
+        while (multiply>1){
+            sum*=multiply;
+            multiply--;
+        }
+        return sum;
     }
 
 
