5602925fbbb973869ea057b4b67e01ed50536fe1
hazelerkmen <144830896+hazelerkmen@users.noreply.github.com>
2023-12-12 13:58:38 -0500

    Add files via upload

--- Factorial.java
+++ Factorial.java
@@ -1,9 +1,11 @@
 public class Factorial {
     public static void main(String[] args){
         Factorial f = new Factorial();
-        for (int n=1; n<=20; n++){
-            System.out.println(f.calcFactorial(n));
-        }
+        //for (int n=1; n<=20; n++){
+        //    System.out.println(f.calcFactorial(n));
+       //}
+        double e = f.calcE();
+        System.out.printf("e is %2.3f\n", e);
     }
 
     public long calcFactorial(int n){
@@ -14,6 +16,26 @@ public class Factorial {
             multiply--;
         }
         return sum;
+    }
+ 
+    public double calcE(){
+        double olde=1.0;
+        double e=2.0;
+        int oldeF=1;
+        int eF=2;
+        while (e-olde>0.001){
+            olde+=1.0/(calcFactorial(oldeF));
+            e+=1.0/(calcFactorial(eF));
+            oldeF++;
+            eF++;
+        }
+        return e;
+    }
+
+    public double calcEX(){
+        double olde=0.0;
+        double e=1.0;
+        int x;
     }
 
 
