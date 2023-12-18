1f79ff8b3e128d88a8ba5432769157670fd2602a
hazelerkmen <144830896+hazelerkmen@users.noreply.github.com>
2023-12-18 13:58:31 -0500

    Add files via upload

---
+++ AdvancedLoops.java
@@ -0,0 +1,42 @@
+public class AdvancedLoops {
+    public static void main(String[] args){
+        AdvancedLoops al=new AdvancedLoops();
+        eights();
+        alternate(5);
+        System.out.println("");
+        alternate(6);
+    }
+
+    public static void eights(){
+        int i;
+        int j;
+        int a;
+        int b=0;
+        for (i=8; i>=2; i-=2){
+            for (a=b; a>0; a--){
+                System.out.print(" ");
+            }
+            for (j=i; j>=1; j--){
+                System.out.print(i);
+            }
+            b++;
+            System.out.println("");
+        }
+    }
+
+    public static void alternate(int n){
+        for (int i=1; i<=n; i++){
+            if (i==1 || i%2!=0){
+                for (int j=1; j<=i; j++){
+                    System.out.print(j);
+                }
+            }
+            else{
+                for (int j=i; j>=1; j--){
+                    System.out.print(j);
+                }
+            }
+            System.out.println("");
+        }
+    }
+}
