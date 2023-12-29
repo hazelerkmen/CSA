b2a22e48189357028c94f4049ccb793bb3d4fcec
Hazel Erkmen <hazelerkmen@billericak12.com>
2023-12-29 14:49:04 +0000

    ugg

--- AdvancedLoops.java
+++ AdvancedLoops.java
@@ -1,14 +1,15 @@
 public class AdvancedLoops {
     public static void main(String[] args){
         AdvancedLoops al=new AdvancedLoops();
-        eights();
-        alternate(5);
-        alternate(6);
-        System.out.println("");
+        al.eights();
+        al.alternate(5);
+        al.alternate(6);
         al.isosceles(5);
+        al.isosceles(3);
+       // al.upSideDown(9, 5);
     }
 
-    public static void eights(){
+    public void eights(){
         for (int i=8; i>=2; i-=2){
             for (int a=(8-i)/2; a>0; a--){
                 System.out.print(" ");
@@ -20,7 +21,7 @@ public class AdvancedLoops {
         }
     }
 
-    public static void alternate(int n){
+    public void alternate(int n){
         for (int i=1; i<=n; i++){
             if (i==1 || i%2!=0){
                 for (int j=1; j<=i; j++){
@@ -38,16 +39,34 @@ public class AdvancedLoops {
     }
 
     public void isosceles(int height){
-        int x=1;
-        for (int i=height; i>0; i--){
-            for (int j=(i-1); j>0; j--){
+        for (int i=1; i<=height; i++){
+            for (int j=height-i; j>0; j--){
                 System.out.print(" ");
             }
-            for (int k=1; k<=x; k++){
+            for (int k=1; k<=i; k++){
                 System.out.print(k);
             }
-            x++;
+            for (int h=i-1; h>0; h--){
+                System.out.print(h);
+            }
             System.out.println("");
         }
+        System.out.println("");
+    }
+
+    public void upSideDown(int big, int small){
+        for (int i=big; i>=small; i--){
+            for (int k=big-i; k>0; k--){
+                System.out.print(" ");
+            }
+            for (int j=i; i>=small; j--){
+                System.out.print(j);
+            }
+            for (int q=small+1; q<=big; q++){
+                System.out.print(q);
+            }
+            System.out.println("");
+        }
+        System.out.println("");
     }
 }
