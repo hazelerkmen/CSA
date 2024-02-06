9ed05455d07f606b83db3035e55b8602f3f67e5e
hazelerkmen <144830896+hazelerkmen@users.noreply.github.com>
2024-02-06 10:22:21 -0500

    Add files via upload

--- Complex.java
+++ Complex.java
@@ -6,5 +6,25 @@ public class Complex {
 //- abs() method
 //- 2 add methods
 //- 2 mult methods
-//- in main(), 6 complex number instances containing a mix of positive and negative values. One of the complex should have only a real part and one should have only an imaginary part.
+//- in main(), 6 complex number instances containing a mix of positive and negative values. 
+//One of the complex should have only a real part and one should have only an imaginary part.
+
+private double firstCoeff;
+private double secondCoeff;
+
+public Complex(double a){
+    firstCoeff=a;
+    secondCoeff=0.0;
+}
+
+public Complex(double a, double b){
+    firstCoeff=a;
+    secondCoeff=b;
+}
+
+public double abs(){
+    return Math.sqrt((firstCoeff*firstCoeff)+(secondCoeff*secondCoeff));
+}
+
+
 }
