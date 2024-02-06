0f9a9488592d7a400b8d0f3f328a5b2777c290db
hazelerkmen <144830896+hazelerkmen@users.noreply.github.com>
2024-02-06 10:21:56 -0500

    Add files via upload

---
+++ Complex.java
@@ -0,0 +1,10 @@
+public class Complex {
+    //The Complex class should contain the following:
+//- 2 instance variables (attributes)
+//- 2 constructors (use this to call one from the other)
+// toString() method
+//- abs() method
+//- 2 add methods
+//- 2 mult methods
+//- in main(), 6 complex number instances containing a mix of positive and negative values. One of the complex should have only a real part and one should have only an imaginary part.
+}
